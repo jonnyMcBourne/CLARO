cd10bd1b094b4cd065d4e3d4c93e7d8f6c871b1c
jonnyMcBourne <72614379+jonnyMcBourne@users.noreply.github.com>
2023-03-31 13:16:09 -0600

    configuring theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { useContext } from 'react';
 import { UIContext } from './context/Ui/UiContext';
 import { Button } from '@mui/material';
+
 import { Box } from '@mui/system';
 import { EPGModal } from './components/';
 
 
 function App ()
 {
-  const { isModalOpen,toggleModal } = useContext(UIContext);
-  console.log(isModalOpen);
+  const { toggleModal } = useContext(UIContext);
   return (
-    <Box sx={{position:'relative'}} >
+    <Box sx={{position:'relative', backgroundColor:'white'}} >
       <Box sx={ { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' } } >
-        <Button variant='contained' onClick={toggleModal}  >Mostrar EPG</Button>
+        <Button variant='contained' onClick={ toggleModal }  >Mostrar EPG</Button>
       </Box>
       <EPGModal/>
     </Box>
